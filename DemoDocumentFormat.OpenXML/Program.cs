c0f98393d5ed2cca5d40c5ab50cfc5ed434f3552
Imir Hoxha <imir.hoxha@gmail.com>
2019-11-17 22:07:31 +0100

    test

--- DemoDocumentFormat.OpenXML/Program.cs
+++ DemoDocumentFormat.OpenXML/Program.cs
@@ -14,12 +14,12 @@ namespace DemoDocumentFormat.OpenXML
         static void Main(string[] args)
         {
             // Apply the Heading 3 style to a paragraph.   
-            string fileName = @"C:\Users\Public\Documents\WordProcessingEx.docx";
+            string fileName = @"C:\Dev\testing.docx";
             using (WordprocessingDocument myDocument = WordprocessingDocument.Open(fileName, true))
             {
                 // Get the first paragraph.  
                 Paragraph p = myDocument.MainDocumentPart.Document.Body.Elements<Paragraph>().First();
-
+                
                 // If the paragraph has no ParagraphProperties object, create a new one.  
                 if (p.Elements<ParagraphProperties>().Count() == 0)
                     p.PrependChild<ParagraphProperties>(new ParagraphProperties());
@@ -29,6 +29,7 @@ namespace DemoDocumentFormat.OpenXML
 
                 // Set the value of ParagraphStyleId to "Heading3".  
                 pPr.ParagraphStyleId = new ParagraphStyleId() { Val = "Heading3" };
+
             }
             Console.WriteLine("All done. Press a key.");
             Console.ReadKey();
